057ca312a01a90b8bf33265e5ba57c47bc059be7
krtkeyan <karthikepsilon@gmail.com>
2017-01-08 12:44:18 +0530

    Normalize State

--- src/Reducers/todos.js
+++ src/Reducers/todos.js
@@ -1,26 +1,35 @@
 import todo from "./todo";
+import {combineReducers} from 'redux'
 
-const todos = function(state=[],action){
+const byId = (state={},action) => {
     switch (action.type) {
         case "ADD-TODO":
-                    return [
-                        ...state,
-                        todo(undefined,action)
-                    ];
         case "TOGGLE":
-                    return state.map((t)=>todo(t,action));
-
+            return Object.assign({},state,{[action.id]:todo(state[action.id],action)})
         default: return state;
-    }
-};
+}
+}
 
+const allIds = (state=[],action) => {
+    switch (action.type){
+        case "ADD-TODO":
+            return [...state,action.id]
+        default:
+            return state
+    }
+}
+const todos = combineReducers({byId,allIds});
 export default todos;
 
+const getAllTodo = (state) =>
+     state.allIds.map(id=>state.byId[id]);
+
 export const getVisiblity = (state,filter) => {
+  const allTodo = getAllTodo(state);
   switch (filter){
-    case "all": return state;
-    case "active": return state.filter(t=>!t.completed);
-    case "completed": return state.filter(t=>t.completed);
+    case "all": return allTodo;
+    case "active": return allTodo.filter(t=>!t.completed);
+    case "completed": return allTodo.filter(t=>t.completed);
     default : throw new Error(`Unknown Filter ${filter}`)
   }
-};
+}
