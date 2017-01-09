9284a211671743b00e09fc522d6c606ec79ecc6a
krtkeyan <karthikepsilon@gmail.com>
2017-01-09 10:19:45 +0530

    Log Actions to Console

--- src/actions/configStore.js
+++ src/actions/configStore.js
@@ -2,9 +2,24 @@ import {throttle} from 'lodash'
 import {loadState,saveState} from './localStorage'
 import {createStore} from "redux";
 import todoList from '../Reducers/todoList';
+
+const addLogging = (store) => {
+  const rawDispatch = store.dispatch;
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action); 
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 export const config = () => {
 const persistedState=loadState();
 const store = createStore(todoList,persistedState);
+store.dispatch = addLogging(store)
 store.subscribe(throttle(()=>{
   saveState({todos:store.getState().todos})
 }),1000)
